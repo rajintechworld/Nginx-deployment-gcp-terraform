d1165ab6eaddadc19fd521ce536ffdd380ebc4d3
rajintechworld <prince.nitskm@gmail.com>
2025-03-29 18:02:21 +0530

    terraform added

---
+++ main.tf
@@ -0,0 +1,12 @@
+# **Terraform: GKE Cluster Setup (main.tf)**
+
+provider "google" {
+  project = "terraform-gcp-455203"  # Replace with your GCP project ID
+  region  = "us-central1"
+}
+
+resource "google_container_cluster" "gke" {
+  name     = "nginx-gke-cluster"
+  location = "us-central1"
+  enable_autopilot = true  # Free-tier eligible control plane
+}
